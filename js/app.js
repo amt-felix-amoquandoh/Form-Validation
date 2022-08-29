63b62213235bd63e9ce8fbed3ea89487d4e318b4
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-08-29 11:57:09 +0000

    form project update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// inputs
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const password = document.getElementById("passKey");
+const confirmPassword = document.getElementById("confirmPassKey");
+const email = document.getElementById("email");
+
+
+//form
