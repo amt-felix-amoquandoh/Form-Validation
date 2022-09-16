55fa78d570298e8c996d406bbc43b5135f157e58
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-09-16 15:44:57 +0000

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let userName = document.getElementById("username");
+let email = document.getElementById("email");
+let passWord = document.getElementById("password");
+let errorMessage = document.getElementsByClassName("errorMsg");
+let valid = document.getElementsByClassName("greenIcon");
+let invalid = document.getElementsByClassName("red");
+
