7c72754f99fbd23492d3ce38d602308e349b053f
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-06 15:38:36 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,13 +7,22 @@ let validIcon = document.getElementsByClassName("greenIcon");
 let invalidIcon = document.getElementsByClassName("redIcon");
 
 
+let formEngine = function (id, number, message){
+    if(id.value.trim() === ""){
+        errorMessage[number].innerHTML = message;
+        invalidIcon[number].style.opacity = "1"
+        validIcon[number].style.opacity = "0"
+    } else{
+        errorMessage[number].innerHTML = "";
+        invalidIcon[number].style.opacity = "0"
+        validIcon[number].style.opacity = "1"
+    }
+}
+
 mainForm.addEventListener("submit", function(e){
     e.preventDefault();
-    if(userName.value === ""){
-        errorMessage[0].innerHTML = "username cannot be blank";
-        invalidIcon[0].style.opacity = "1"
-    } else{
-        errorMessage[0].innerHTML = ""
-    }
+    formEngine(userName, 0, "username cannot be blank");
+    formEngine(email, 1, "email should not be blank");
+    formEngine(passWord, 2, "password must contain letter and numbers");
 })
 
