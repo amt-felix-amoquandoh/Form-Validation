22e8e68853b7e9b7941c69e2fb79d524e09da889
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-06 15:09:43 +0000

    project files update

--- js/app.js
+++ js/app.js
@@ -3,11 +3,17 @@ let userName = document.getElementById("username");
 let email = document.getElementById("email");
 let passWord = document.getElementById("password");
 let errorMessage = document.getElementsByClassName("errorMsg");
-let valid = document.getElementsByClassName("greenIcon");
-let invalid = document.getElementsByClassName("red");
+let validIcon = document.getElementsByClassName("greenIcon");
+let invalidIcon = document.getElementsByClassName("redIcon");
 
 
-mainForm.addEventListener("submit", function(){
-    errorMessage[2].innerHTML = "fuuuuuuuuuucccc"
+mainForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    if(userName.value === ""){
+        errorMessage[0].innerHTML = "username cannot be blank";
+        invalidIcon[0].style.opacity = "1"
+    } else{
+        errorMessage[0].innerHTML = ""
+    }
 })
 
